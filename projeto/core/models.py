8f922ef91b13f489e4c15a6e3105f859ecf20886
Nando <luisfgiglio@gmail.com>
2023-06-09 15:28:52 -0300

    Create migrations

--- projeto/core/models.py
+++ projeto/core/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+MOVEMENT = (
+    ('e', 'entrada'),
+    ('s', 'saida'),
+)
+class TimeLineModel(models.Model):
+    created = models.DateTimeField(verbose_name=('Criando em'), auto_now_add=True, auto_now=False)
+    modified = models.DateTimeField(verbose_name=('Alterado em'), auto_now_add=False, auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class ProductModel(models.Model):
+    product = models.CharField(verbose_name=('Nome'), max_length=100, unique=True)
+    price = models.DecimalField(verbose_name=('Preço'), max_digits=7, decimal_places=2)
+    stock_current = models.IntegerField(verbose_name=('Estoque Atual'))
+    stock_mini = models.PositiveIntegerField(verbose_name=('Estoque mínimo'), default=0)
+
+    def __str__(self):
+        return self.product
+
+class StockModel(TimeLineModel):
+    employee = models.ForeignKey(User, verbose_name=('Funcionario'), on_delete=models.CASCADE)
+    invoice = models.PositiveIntegerField(verbose_name=('Nota Fiscal'), null=True, blank=True)
+    movement = models.CharField(max_length=1, choices=MOVEMENT)
+    
+    def __str__(self):
+        return str(self.pk)
+
+class StockLineModel(models.Model):
+    stock = models.ForeignKey(StockModel, on_delete=models.CASCADE)
+    product = models.ForeignKey(ProductModel, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField()
+    balance = models.PositiveIntegerField()
+    
+    def __str__(self):
+        return f'{self.pk}-{self.stock}-{self.product}'
